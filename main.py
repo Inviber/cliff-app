b6d129deb55504dbf3f88f1290ab5f201ef18f18
Inviber <bkelley4600@gmail.com>
2024-05-21 19:14:19 -0500

    Added choice menu for user and welcome ascii art

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 from repformulas import epleyCalculation, brzyckiCalculation, landerCalculation
 
+def generateWelcomeMessage():
+    print(r"""     
+   __   __    __  ____
+ ,'_/  / /   / / / __/
+/ /_  / /_  / / / _/  
+|__/ /___/ /_/ /_/    
+                      
+""")
+
+def menu():
+    print("\nThis Fitness application is responsible for accomplishing: ")
+    print("\t1. Generating One Rep Max (Best for Reps 10 and under)")
+    print("\t2. Macro Calculations per individual")
+    print("\t3. Generating a basic workout program")
+    choice = input("\nWhat would you like to do? (1-3) : ")
+    return choice
+
 def main():
-    weight = input("What is your weight? (lbs only) : ")
-    reps = input("What is the number of reps for the exercise? \n(Lower the reps the more accurate it would be) : ")
+    generateWelcomeMessage()
+    choice = menu()
+    print(choice)
+    weight = input("\nWhat is your weight? (lbs only) : ")
+    reps = input("\nWhat is the number of reps for the exercise? \n(Lower the reps the more accurate it would be) : ")
     print(f"Your weight is {weight} lbs")
     print(f"The number of reps reported is {reps}")
 
